5b3d6099364e3c7ce2bce9253e55a63e0518f3e6
gtakat <gtakat@gmail.com>
2017-08-02 10:38:19 +0900

    no any (state)

--- src/containers/Goban.ts
+++ src/containers/Goban.ts
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import { Goban } from '../components/Goban'
 import { IAppState } from '../constants/StateTypes'
 
-const mapStateToProps = (state: IAppState): any => (state)
+const mapStateToProps = (state: IAppState): IAppState => (state)
 
 const mapDispatchToProps = (dispatch: any): any => ({})
 
